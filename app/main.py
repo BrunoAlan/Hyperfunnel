7c82824ff5cd6f095b38ae901636818b4bfc0534
BrunoAlan <alan.brunoab@gmail.com>
2025-08-20 20:26:01 -0300

    Remove initialization SQL script and related Docker configurations
    - Deleted the init.sql file that contained database initialization scripts for the PostgreSQL container.
    - Updated the Dockerfile and docker-compose.yml to remove references to the init.sql file, streamlining the container setup process.
    - This change simplifies the database initialization process, relying on environment variables for database setup instead.

--- app/main.py
+++ app/main.py
@@ -24,3 +24,9 @@ app.include_router(destinations.router)
 @app.get("/")
 def read_root():
     return {"message": "Hyper funnel api"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
